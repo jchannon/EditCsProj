ad972cf68667c6c957aec48e358720c03eaf92e3
Jonathan Channon <jonathan.channon@gmail.com>
2018-04-16 10:52:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Edit CsProj
+
+This tool allow you to add and update information inside a csproj file.
+
+### Usage
+
+
+#### Adding element to `<PropertyGroup>`
+
+This will add `<Test>1</Test>` to the first `<PropertyGroup>` in the csproj
+
+`editcsproj -f Foo.csproj -ap "<Test>1</Test>"`
+
+#### Updating attribute
+
+This will update an existing attribute. In this example we tell it to search for elements called `PackageReference`, we then filter those down by searching for attributes called `Include` whose value is `FluentValidation`, we then set the `Version` attribute to the value of `9.1.2`.
+
+`editcsproj -f Foo.csproj updateattribute PackageReference Include FluentValidation Version 9.1.2`
+
+#### Updating element
+
+This will update an exisgint element.
+
+`editcsproj -f Foo.csproj updateelement Version 9.1.2`
+
+#### Add element to existing `<ItemGroup>`
+
+This will add an element to an existing `<ItemGroup>`
+
+`editcsproj -f Foo.csproj -ae "<Test Foo=\"Bar\" />`
+
+#### Add element to new `<ItemGroup>`
+
+This will add an element and create a new `<ItemGroup>` in the csproj
+
+`editcsproj -f Foo.csproj -ai "<Test Foo=\"Bar\" />`
+
---
+++ src/EditCsProj/Program.cs
@@ -0,0 +1,120 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace EditCsProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.HelpOption();
+
+            var addExisting = app.Option("-ae|--addexisting <element>", "Add an xml element to existing ItemGroup", CommandOptionType.MultipleValue);
+            var addNew = app.Option("-ai|--add <element>", "Add an xml element to new ItemGroup", CommandOptionType.MultipleValue);
+            var addNewToPropertyGroup = app.Option("-ap|--addpropertygroup <element>", "Add an xml element to existing PropertyGroup", CommandOptionType.MultipleValue);
+
+            var filePath = app.Option("-f|--file <csproj>", "The path to the csproj", CommandOptionType.SingleValue).IsRequired().Accepts(builder => builder.ExistingFile());
+
+            app.Command("updateattribute", updateCmd =>
+            {
+                var nameElementArgument = updateCmd.Argument("elementname", "Name of element of where attribute exists").IsRequired();
+                var attributeNameArgument = updateCmd.Argument("attributename", "Name of attribute to identify").IsRequired();
+                var attributeNameValueArgument = updateCmd.Argument("attributevalue", "Value of named attribute to filter on").IsRequired();
+
+                var nameArgument = updateCmd.Argument("name", "Name of attribute to update with given value").IsRequired();
+                var valueArgument = updateCmd.Argument("value", "Value to set the attribute").IsRequired();
+
+                updateCmd.HelpOption();
+
+                updateCmd.OnExecute(() =>
+                {
+                    var csProjPath = filePath.Value();
+                    var xmlDoc = XDocument.Load(csProjPath);
+                    xmlDoc.Descendants(nameElementArgument.Value).FirstOrDefault(x => x.Attribute(attributeNameArgument.Value).Value == attributeNameValueArgument.Value).Attribute(nameArgument.Value)
+                        .Value = valueArgument.Value;
+                    xmlDoc.Save(csProjPath);
+                    return 0;
+                });
+            });
+
+            app.Command("updateelement", updateCmd =>
+            {
+                var nameArgument = updateCmd.Argument("name", "Name of element to update with given value").IsRequired();
+                var valueArgument = updateCmd.Argument("value", "Value to set the element").IsRequired();
+
+                updateCmd.HelpOption();
+
+                updateCmd.OnExecute(() =>
+                {
+                    var csProjPath = filePath.Value();
+                    var xmlDoc = XDocument.Load(csProjPath);
+                    xmlDoc.Descendants(nameArgument.Value).First().Value = valueArgument.Value;
+                    xmlDoc.Save(csProjPath);
+                    return 0;
+                });
+            });
+
+            app.OnExecute(() =>
+            {
+                XDocument xmlDoc = null;
+
+                if (addExisting.HasValue() || addNew.HasValue() || addNewToPropertyGroup.HasValue())
+                {
+                    xmlDoc = XDocument.Load(filePath.Value());
+                }
+
+                var changed = false;
+                if (addNew.HasValue())
+                {
+                    var element = new XElement("ItemGroup");
+
+                    foreach (var newValue in addNew.Values)
+                    {
+                        var ele = XElement.Parse(newValue);
+                        element.Add(ele);
+                    }
+
+                    xmlDoc.Root.Add(element);
+                    changed = true;
+                }
+
+                if (addExisting.HasValue())
+                {
+                    var element = xmlDoc.Descendants("ItemGroup").Last();
+
+                    foreach (var newValue in addExisting.Values)
+                    {
+                        var ele = XElement.Parse(newValue);
+                        element.Add(ele);
+                    }
+
+                    changed = true;
+                }
+                
+                if (addNewToPropertyGroup.HasValue())
+                {
+                    var element = xmlDoc.Descendants("PropertyGroup").First();
+
+                    foreach (var newValue in addNewToPropertyGroup.Values)
+                    {
+                        var ele = XElement.Parse(newValue);
+                        element.Add(ele);
+                    }
+
+                    changed = true;
+                }
+
+                if (changed)
+                {
+                    xmlDoc.Save(filePath.Value());
+                }
+
+                return 0;
+            });
+
+            app.Execute(args);
+        }
+    }
+}
